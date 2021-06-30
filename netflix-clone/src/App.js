7466f4683ac9d87c00a2d329c3224988c01f1872
JaeHun No <nojahoon@naver.com>
2021-07-01 06:28:30 +0900

    make requests.js/Row.js/axios.js  - to use API

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Row from "./Row";
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Clever Programmer! Let's build Netflix Clone Front-end today</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
     </div>
   );
 }
---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Row({ title }) {
+  return (
+    <div>
+      {/*title*/}
+      <h2>{title}</h2>
+
+      {/*con tainer -> posters */}
+    </div>
+  );
+}
+
+export default Row;
