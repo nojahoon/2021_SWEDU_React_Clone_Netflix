daf0a56bfc6a76e0651a236d9bc45e8adf047ba5
JaeHun No <nojahoon@naver.com>
2021-07-01 18:36:49 +0900

    Error 잡히지는 않지만 CDT에서  Failed to load resource: the server responded with a status of 404 () 로 source 확인불가

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./cssfile/App.css";
 import Row from "./Row";
 import requests from "./requests";
-
+import Banner from "./Banner";
 function App() {
   return (
-    <div className="App">
-      <h1>Hey Clever Programmer! Let's build Netflix Clone Front-end today</h1>
+    <div className="app">
+      {/* Nav */}
+      <Banner />
+
+      {/* Banner */}
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
---
+++ netflix-clone/src/Banner.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import requests from "./requests";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      ); // [... , ... , ...]
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <header>
+      {/* <<< Background Image */}
+
+      {/* title */}
+      {/* div > 2 buttons */}
+      {/* descriptions */}
+    </header>
+  );
+}
+
+export default Banner;
