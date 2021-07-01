8fabcd3b1c72f271722787266eaa9b47d60166de
JaeHun No <nojahoon@naver.com>
2021-07-01 15:30:01 +0900

    - css and js file is in separate directory - change import css path
    - Row.js : add async function
    - request.js : revise '/'  - omission in varibale(fetchNetflixOriginals)
    - App.js : Each category inserted

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import "./App.css";
+import "./cssfile/App.css";
 import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Clever Programmer! Let's build Netflix Clone Front-end today</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./cssfile/row.css";
 
-function Row({ title }) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+  //
+  const [movies, setMovies] = useState([]);
+
+  // A snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+    //if [] , run once when the row loads and don't run again
+    //if [variable] , run every singlie time variable changes
+  }, [fetchUrl]);
+  console.table(movies);
+
   return (
-    <div>
+    <div className="row">
       {/*title*/}
       <h2>{title}</h2>
-
+      <div className="row__posters">
+        {/*several row__poster(s) */}
+        {movies.map((movie) => (
+          //"*****.jpg"
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
       {/*con tainer -> posters */}
     </div>
   );
